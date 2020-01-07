40d312077ca46504514a54aea6c5436ecb41b18f
たけだ <ichigo_happy@ezweb.ne.jp>
2020-01-07 14:42:26 +0900

    read and write file

--- miura_kadai2/Program.cs
+++ miura_kadai2/Program.cs
@@ -12,19 +12,21 @@ namespace miura_kadai2
         static void Main(string[] args)
         {
             string filepath = "../../Student.java";
-            FileStream fs = new FileStream(filepath, FileMode.Open, FileAccess.Read);
-            StreamReader sr = new StreamReader(fs);
-            var writer = new StreamWriter("../../class.txt", true);
+            StreamReader sr = new StreamReader(filepath);
+            Encoding enc = Encoding.GetEncoding("Shift_JIS");
+            var writer = new StreamWriter("../../class.txt", false,enc);
             string text = sr.ReadToEnd();
             sr.Close();
-            fs.Close();
             string[] texts = text.Split();
             for (var i = 0; i < texts.Length; ++i)
             {
-                Console.Write(texts[i] + " ");
-                writer.WriteLine(texts[i]);
+                if(texts[i].Contains( "("))
+                {
+                    Console.WriteLine(texts[i]);
+                    writer.WriteLine(texts[i]);
+                }
             }
-
+            writer.Close();
         }
     }
 }
