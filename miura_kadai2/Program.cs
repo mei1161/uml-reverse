62a723bc3bf933ccbd54dffa237436aa75e584dc
たけだ <ichigo_happy@ezweb.ne.jp>
2020-01-07 11:21:39 +0900

    work in progress

--- miura_kadai2/Program.cs
+++ miura_kadai2/Program.cs
@@ -14,21 +14,17 @@ namespace miura_kadai2
             string filepath = "../../Student.java";
             FileStream fs = new FileStream(filepath, FileMode.Open, FileAccess.Read);
             StreamReader sr = new StreamReader(fs);
+            var writer = new StreamWriter("../../class.txt", true);
             string text = sr.ReadToEnd();
             sr.Close();
             fs.Close();
             string[] texts = text.Split();
-            for(var i=0;i<texts.Length;++i)
+            for (var i = 0; i < texts.Length; ++i)
             {
-                if(texts[i]=="class")
-                {
-                    while(texts[i++]!="}")
-                    {
-                        Console.Write(texts[i]+" ");
-                    }
-                }
-
+                Console.Write(texts[i] + " ");
+                writer.WriteLine(texts[i]);
             }
+
         }
     }
 }
